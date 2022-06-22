6441f26bf92a2d2fb0b2f0975e36069021eb2f7c
Renaud <fournet.renaud@gmail.com>
2022-06-22 17:00:18 +0200

    fixed lottie

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,7 +16,7 @@ function Header() {
 
   return (
     <>
-      <div className="ml-96" ref={container}></div>
+      <div className="ml-96 p-10" ref={container}></div>
       <p className="text-white-100 font-bold text-image text-left ml-5 xs:ml-10 sm:ml-10 md:ml-16 lg:ml-24 mb-14 xxs:mb-0 xs:mb-10 sm:mb-14 md:mb-0">
         <span className="text-2xl xs:text-3xl sm:text-3xl md:text-3xl lg:text-4xl">
           Explores the price of
